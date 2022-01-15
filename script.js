4a4d9f8dc40cb1e4f08107f3f0d7c765294bbab3
Paula <p.a.m@live.com>
2022-01-15 18:34:59 -0300

    Implementar script no arquivo html.

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.scr = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name
+        especie.innerHTML = data.especies
+        condicao.innerHTML = data.status
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
