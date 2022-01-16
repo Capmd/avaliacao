61823bde0ffa1244d0967b589ea2af2877ee7af3
Paula <p.a.m@live.com>
2022-01-16 10:17:33 -0300

    Adicionar três variáveis ao arquivo js para implementar o html.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ gerarValorAleatorio = () => {
     return Math.floor(Math.random() *671);
 }
 
+
 pegarPersonagem = () => {
     let numeroAleatorio = gerarValorAleatorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
@@ -17,13 +18,67 @@ pegarPersonagem = () => {
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
+        imagem.src = data.image;        
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name
-        especie.innerHTML = data.especies
+        especie.innerHTML = data.species
         condicao.innerHTML = data.status
 
     });
 }
 
-botao.onclick = pegarPersonagem;
+
+const imagem2 = document.querySelector('#img2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+
+pegarPersonagem2 = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;        
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name
+        especie2.innerHTML = data.species
+        condicao2.innerHTML = data.status
+
+    });
+}
+
+const imagem3 = document.querySelector('#img3');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
+
+pegarPersonagem3 = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;        
+        imagem3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name
+        especie3.innerHTML = data.species
+        condicao3.innerHTML = data.status
+
+    });
+}
+
+var concatenar = () => {
+    pegarPersonagem();
+    pegarPersonagem2();
+    pegarPersonagem3();
+}
+
+
+botao.onclick = concatenar
